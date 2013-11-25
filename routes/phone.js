624a5cae7cd5e262720312f7333e37625b26d708
Mike Kunkel <mike@mkunkel.com>
2013-11-25 11:32:55 -0600

    fixed params reference

--- routes/phone.js
+++ routes/phone.js
@@ -1,5 +1,5 @@
 
 // GET /phone/:id
 exports.index = function(req, res){
-  res.render('phone/index', {title: 'Pong 2.0', player: res.locals.player, playerId: req.session.playerId});
+  res.render('phone/index', {title: 'Pong 2.0', player: res.locals.player, playerId: req.params.id});
 };
